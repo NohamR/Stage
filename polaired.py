5e17777ab5f129bd6fb32bed19db9ca6b70d295f
√(noham)² <noham.rivoirard@gmail.com>
2023-07-18 16:43:53 +0200

    changes

--- polaired.py
+++ polaired.py
@@ -20,26 +20,6 @@ def rainbow_gradient(num_colors):
     return colors
 colors = rainbow_gradient(nbv)
 
-def status(distances):
-    num_colors = len(distances)
-    colors = []
-    base_color = Color("green")
-    target_color = Color("red")
-    luminance_start = base_color.get_luminance()
-    luminance_end = target_color.get_luminance()
-    for i in range(num_colors):
-        moydist = distances[i]
-        t = i / (num_colors - 1)
-        adjusted_luminance = luminance_start + (luminance_end - luminance_start) * (1 - t) * (moydist - 1) / 18
-        color = Color(rgb=(base_color.rgb[0] * (1 - t) + target_color.rgb[0] * t,
-                           base_color.rgb[1] * (1 - t) + target_color.rgb[1] * t,
-                           base_color.rgb[2] * (1 - t) + target_color.rgb[2] * t))
-        color.set_luminance(adjusted_luminance)
-        hex_code = color.hex_l
-        colors.append(hex_code)
-    return colors
-
-
 U = 1.25 # vitesse m.s-¹
 Wm = 0.3 # distance minimale entre la voiture et celle qui la précède m
 Ws = 0.9 # m
@@ -64,34 +44,24 @@ def position(fposition, newv):
 
 xxold = xxbase.copy()
 
-while(t<tf):
-    plt.figure(1,figsize=[16,9])
+while(t < tf):
+    plt.figure(1, figsize=[16, 9])
     plt.clf()
 
     nb = 360
+    r = np.linspace(1, 1, nb)
+    theta = np.linspace(0, 2 * np.pi, nb)
 
-    r=np.linspace(1,1,nb)
-    theta=np.linspace(0,2*np.pi,nb)
-
-    plt.polar(theta, r)
-    # plt.scatter(1*np.pi, 0.5)
+    plt.polar(theta, r, alpha=0)
 
     dst = distances(xxold)
-    statusc = status(dst)
-    print(statusc)
 
     vt = phi(dst)
-    # print('vitesses : ', vt)
-
     xx = position(xxold, vt)
-    # print('position : ', xx)
 
-    plt.scatter(xx/10*np.pi, y, c=colors)
+    plt.scatter(xx/10 * np.pi, y, c=colors)
 
-    for i in range(len(xx)):
-        plt.plot(xx[i]/10*np.pi, y[i], color=statusc[i])
-
-    plt.title('Vitesse maximale : ' + str(U) + 'm.s-¹\ndistance minimale entre deux voitures : ' + str(Wm) + 'm\nnombre de voitures : ' + str(nbv))
+    plt.title('Vitesse maximale : ' + str(U) + ' m/s\ndistance minimale entre deux voitures : ' + str(Wm) + ' m\nnombre de voitures : ' + str(nbv))
     plt.draw()
     plt.pause(0.00001)
     t += dt
